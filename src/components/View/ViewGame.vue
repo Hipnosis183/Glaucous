34ece0a5896dd02de010ac4847e2fe33f3d5fca3
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2021-06-25 14:44:49 -0300

    Added buttons to switch between the regions of a game

--- src/components/View/ViewGame.vue
+++ src/components/View/ViewGame.vue
@@ -11,12 +11,12 @@
         }"
       >New Region</router-link>
     </button>
-    <p>{{ game.gameRegions[0].title }}</p>
-    <p>{{ game.gameRegions[0].subTitle }}</p>
-    <p>{{ game.gameRegions[0].originalTitle }}</p>
-    <p>{{ game.gameRegions[0].romanizedTitle }}</p>
-    <p>{{ game.gameRegions[0].translatedTitle }}</p>
-    <p>{{ game.gameRegions[0].region }}</p>
+    <p>{{ game.gameRegions[region].title }}</p>
+    <p>{{ game.gameRegions[region].subTitle }}</p>
+    <p>{{ game.gameRegions[region].originalTitle }}</p>
+    <p>{{ game.gameRegions[region].romanizedTitle }}</p>
+    <p>{{ game.gameRegions[region].translatedTitle }}</p>
+    <p>{{ game.gameRegions[region].region }}</p>
     <p @click="$router.push(`/developers/${ game.developer._id }`)"
     >{{ game.developer.name }}</p>
     <p @click="$router.push(`/platforms/${ game.platform._id }`)"
@@ -24,8 +24,11 @@
     <p>{{ game.releaseYear }}</p>
     <p>{{ game.numberPlayers }}</p>
     <p>{{ game.latestVersion }}</p>
-    <p>{{ game.gameRegions[0].gameVersions[0].currentVersion }}</p>
-    <p>{{ game.gameRegions[0].gameVersions[0].comments }}</p>
+    <p>{{ game.gameRegions[region].gameVersions[0].currentVersion }}</p>
+    <p>{{ game.gameRegions[region].gameVersions[0].comments }}</p>
+    <button @click="prevRegion()">Prev</button>
+    <button @click="nextRegion()">Next</button>
+    <br>
     <button @click="$router.back()">Back</button>
   </div>
 </template>
@@ -40,6 +43,7 @@
     name: 'view-game',
     data () {
       return {
+        region: 0,
         game: {
           developer: { name: null },
           platform: { name: null },
@@ -62,7 +66,18 @@
         }
       }
     },
-    methods: {},
+    methods: {
+      nextRegion() {
+        if (this.region < this.game.gameRegions.length - 1) {
+          this.region++
+        }
+      },
+      prevRegion() {
+        if (this.region > 0) {
+          this.region--
+        }
+      }
+    },
     mounted () {
       connectDatastore(this.$route.query.p).then(() => {
         getGame(this.$route.params.id).then(res => {
