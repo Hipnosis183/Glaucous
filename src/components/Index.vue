1fb2b7ee0cdf8fbe09525b263daf8da89c959ddf
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2021-06-25 18:25:31 -0300

    Setup Tailwind with PostCSS

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <button>
-      <router-link to="/games">Games</router-link>
-    </button>
-    <button>
-      <router-link to="/developers">Developers</router-link>
-    </button>
-    <button>
-      <router-link to="/platforms">Platforms</router-link>
-    </button>
+  <div class="flex space-x-2">
+    <router-link to="/games" class="w-1/3">
+      <button class="w-full bg-gray-200 text-blue-800 text-base font-semibold px-6 py-2 rounded-full">Games</button>
+    </router-link>
+    <router-link to="/developers" class="w-1/3">
+      <button class="w-full bg-gray-200 text-blue-800 text-base font-semibold px-6 py-2 rounded-full">Developers</button>
+    </router-link>
+    <router-link to="/platforms" class="w-1/3">
+      <button class="w-full bg-gray-200 text-blue-800 text-base font-semibold px-6 py-2 rounded-full">Platforms</button>
+    </router-link>
   </div>
 </template>
 
