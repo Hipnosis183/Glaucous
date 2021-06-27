aa4a8d2f376605c46745e1001919e36852163a54
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2021-06-27 05:25:59 -0300

    Sidenav: Added collapsing/expanding functionality

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,17 +1,56 @@
 <template>
-  <div class="w-60 bg-indigo-500 space-y-4 text-center">
-    <h1 class="text-gray-200 font-semibold text-3xl my-8">Frontend</h1>
-    <router-link to="/games" class="flex">
-      <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold px-6 py-3 rounded-full">Games</button>
-    </router-link>
-    <router-link to="/developers" class="flex">
-      <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold px-6 py-3 rounded-full">Developers</button>
-    </router-link>
-    <router-link to="/platforms" class="flex">
-      <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold px-6 py-3 rounded-full">Platforms</button>
-    </router-link>
+  <div class="transform ease-in-out transition-all duration-1000 relative bg-indigo-500 flex-col justify-between flex" :class="expanded ? 'w-60' : 'w-14'">
+    <div class="h-full flex-col justify-between flex">
+      <div class="text-center">
+        <h1 class="text-gray-200 font-semibold text-3xl my-12">{{ name }}</h1>
+        <div class="space-y-4 mt-8">
+          <router-link to="/games" class="flex">
+            <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold py-3 rounded-full">{{ games }}</button>
+          </router-link>
+          <router-link to="/developers" class="flex">
+            <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold py-3 rounded-full">{{ developers }}</button>
+          </router-link>
+          <router-link to="/platforms" class="flex">
+            <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold py-3 rounded-full">{{ platforms }}</button>
+          </router-link>
+        </div>
+      </div>
+      <div class="flex mb-8">
+        <button class="w-10/12 m-auto bg-gray-200 text-blue-800 text-base font-semibold py-3 rounded-full">{{ settings }}</button>
+      </div>
+    </div>
+    <div class="bg-indigo-600 flex text-center">
+      <button class="text-gray-200 font-semibold text-2xl m-auto py-3" :class="expanded ? 'mr-auto' : 'mr-5'" @click="toggle()">{{ icon }}</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      expanded: true,
+      name: 'Frontend',
+      icon: '<',
+      games: 'Games',
+      developers: 'Developers',
+      platforms: 'Platforms',
+      settings: 'Settings'
+    } 
+  },
+  methods: {
+    toggle() {
+      this.expanded = !this.expanded;
+      this.name = this.expanded ? 'Frontend' : 'F'
+      this.icon = this.expanded ? '<' : '>'
+      this.games = this.expanded ? 'Games' : 'G'
+      this.developers = this.expanded ? 'Developers' : 'D'
+      this.platforms = this.expanded ? 'Platforms' : 'P'
+      this.settings = this.expanded ? 'Settings' : 'S'
+    }
+  }
+}
+</script>
 
 <style>
 </style>
