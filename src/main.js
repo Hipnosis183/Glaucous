cd1fd1f52e0c1761313e8d3f7cac06e1ccc630f9
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2021-09-22 15:05:21 -0300

    Remove ElementPlus as a dependency

--- src/main.js
+++ src/main.js
@@ -12,10 +12,7 @@ const emitter = mitt()
 // Import UI and icon components.
 import hipnoUI from '@/plugins/HipnosisUI'
 import hipnoIcon from '@/plugins/HipnosisIcon'
-// Import Element Plus.
-import element from 'element-plus'
-import 'element-plus/dist/index.css'
 
-const app = createApp(App).use(store).use(router).use(hipnoUI).use(hipnoIcon).use(element)
+const app = createApp(App).use(store).use(router).use(hipnoUI).use(hipnoIcon)
 app.config.globalProperties.emitter = emitter
 app.mount('#app')
