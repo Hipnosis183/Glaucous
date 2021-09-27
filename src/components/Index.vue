02a2df530127003cd9f3db43aef54933f26c529f
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2021-09-27 00:32:21 -0300

    Remove database seeder

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,19 +1,11 @@
 <template>
-  <div class="flex h-full items-center justify-center">
-    <hip-button-sb @click="dbSeed()">Seed</hip-button-sb>
-  </div>
 </template>
 
 <script>
-// Import database functions.
-import { dbSeed } from '@/database/seeder'
-
 export default {
   name: 'Index',
   setup() {
-    return {
-      dbSeed
-    }
+    return {}
   }
 }
 </script>
